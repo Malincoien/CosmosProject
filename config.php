844345f537b2111c45ed690a38e910297706e16d
Malincoien <malin.oien9@gmail.com>
2023-05-26 11:54:27 +0200

    jhfuhe

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 //Koble til databasen
-$dbc = mysqli_connect('localhost', 'malin', 'cosmos127', 'cosmos')
+$dbc = mysqli_connect('10.2.2.237', 'admin', 'cosmos127', 'cosmos')
 or die('Error connecting to MySQL server.');
 
 
