7dfd9c23cd0cef77c5b14b76912a5bb33bde0214
Malincoien <malin.oien9@gmail.com>
2023-05-27 18:20:16 +0200

    header

--- shop.php
+++ shop.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,300,0,0" />
+    <script src="https://kit.fontawesome.com/da8d2380f3.js" crossorigin="anonymous"></script>
     <title>Products</title>
 </head>
 <body>
